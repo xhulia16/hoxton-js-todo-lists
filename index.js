fa89f17b5c8e436dfb819747ce6c9df4e54c2305
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-17 01:50:17 +0200

    part 1

--- index.js
+++ index.js
@@ -94,50 +94,56 @@ const users = [
 ]
 const todos = [
     {
-      userId: 1,
-      id: 1,
-      title: 'delectus aut autem',
-      completed: false
+        userId: 1,
+        id: 1,
+        title: 'delectus aut autem',
+        completed: false
     },
     {
-      userId: 1,
-      id: 2,
-      title: 'quis ut nam facilis et officia qui',
-      completed: false
+        userId: 1,
+        id: 2,
+        title: 'quis ut nam facilis et officia qui',
+        completed: false
     },
-   { userId: 2,
-    id: 3,
-    title: 'suscipit repellat esse quibusdam voluptatem incidunt',
-    completed: false
-  },
-  {
-    userId: 2,
-    id: 4,
-    title: 'distinctio vitae autem nihil ut molestias quo',
-    completed: true
-  },
-  {
-    userId: 3,
-    id: 5,
-    title: 'rerum perferendis error quia ut eveniet',
-    completed: false
-  },
-  {
-    userId: 3,
-    id: 6,
-    title: 'tempore ut sint quis recusandae',
-    completed: true
-  },
-  {
-    userId: 4,
-    id: 7,
-    title: 'odit optio omnis qui sunt',
-    completed: true
-  },
-  {
-    userId: 4,
-    id: 8,
-    title: 'et placeat et tempore aspernatur sint numquam',
-    completed: false
-  }
+    {
+        userId: 2,
+        id: 3,
+        title: 'suscipit repellat esse quibusdam voluptatem incidunt',
+        completed: false
+    },
+    {
+        userId: 2,
+        id: 4,
+        title: 'distinctio vitae autem nihil ut molestias quo',
+        completed: true
+    },
+    {
+        userId: 3,
+        id: 5,
+        title: 'rerum perferendis error quia ut eveniet',
+        completed: false
+    },
+    {
+        userId: 3,
+        id: 6,
+        title: 'tempore ut sint quis recusandae',
+        completed: true
+    },
+    {
+        userId: 4,
+        id: 7,
+        title: 'odit optio omnis qui sunt',
+        completed: true
+    },
+    {
+        userId: 4,
+        id: 8,
+        title: 'et placeat et tempore aspernatur sint numquam',
+        completed: false
+    }
 ]
+let usersDetails = []
+let message=``
+for (let i = 0; i < users.length; i++) {
+    alert(`Id number: ${users[i].id}; Name: ${users[i].name}; City: ${users[i].address.city}`)
+}
